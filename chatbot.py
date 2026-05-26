edcd3292865026c6b47c2aa554469f37fd7ab832
jafarareebha <jafarareebha@gmail.com>
2026-05-26 17:09:18 +0530

    chatbot

---
+++ chatbot.py
@@ -0,0 +1,15 @@
+def Chatbot():
+    UserInput = input("You:")
+    while UserInput!="bye":
+        if UserInput=="hello" or UserInput=="hi":
+            print("Hi")
+        elif UserInput=="how are you":
+            print("I'm fine! Thanks")
+        elif UserInput=="bye":
+            print("Goodbye!")
+            exit()
+        else:
+            print("Sorry, I don't understand.")
+        UserInput = input("You:")
+    print("Goodbye!")
+Chatbot()
